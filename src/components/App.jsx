47125321a93481ad006c9131bf1fcecafe58b37c
hanaf <hanaf.othmane@gmail.com>
2021-01-24 13:23:23 -0500

    using spread

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,33 +8,17 @@ function App() {
     email: ""
   });
 
-  function handleChange(event){
+  function handleChange(event) {
+
     const {name, value} = event.target;
 
     setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value, 
-          lName: prevValue.lName, 
-          email: prevValue.email
+      return {
+          ...prevValue,
+          [name]: value
         };
-      } 
-      else if (name === "lName") {
-        return {
-          fName: prevValue.fName, 
-          lName: value,
-          email: prevValue.email
-        };
-      } 
-      else if (name === "email") {
-        return ({
-          fName: prevValue.fName, 
-          lName: prevValue.lName, 
-          email: value
-        });
-      }
-    })
-  }
+      });
+    }
 
   return (
     <div className="container">
